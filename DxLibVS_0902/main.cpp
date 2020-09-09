376a1296fb2644157167750178f75f67800fafaf
Sawamdean3 <hj-g19010@sist.ac.jp>
2020-09-09 10:36:37 +0900

    文字コードをマルチバイトに戻した

--- DxLibVS_0902/main.cpp
+++ DxLibVS_0902/main.cpp
@@ -1,65 +1,65 @@
-//########## ヘッダーファイル読み込み ##########
+//########## �w�b�_�[�t�@�C���ǂݍ��� ##########
 #include "DxLib.h"
 
-#define GAME_WIDTH			640	//画面の横の大きさ
-#define GAME_HEIGHT			480	//画面の縦の大きさ
-#define GAME_COLOR			32	//画面のカラービット
+#define GAME_WIDTH			640	//��ʂ̉��̑傫��
+#define GAME_HEIGHT			480	//��ʂ̏c�̑傫��
+#define GAME_COLOR			32	//��ʂ̃J���[�r�b�g
 
-#define GAME_WINDOW_BAR		0	//タイトルバーはデフォルトにする
-#define GAME_WINDOW_NAME	"Dxlib_Movie"	//ウィンドウのタイトル
+#define GAME_WINDOW_BAR		0	//�^�C�g���o�[�̓f�t�H���g�ɂ���
+#define GAME_WINDOW_NAME	"Dxlib_Movie"	//�E�B���h�E�̃^�C�g��
 
-//MOVIEフォルダとmp4ファイルも追加
-#define MOVIE_PATH			".\\MOVIE\\neko.mp4"	//動画のパス
+//MOVIE�t�H���_��mp4�t�@�C�����ǉ�
+#define MOVIE_PATH			".\\MOVIE\\neko.mp4"	//����̃p�X
 
-int handle = -1;	//動画のハンドル
+int handle = -1;	//����̃n���h��
 
 
-//########## プログラムで最初に実行される関数 ##########
+//########## �v���O�����ōŏ��Ɏ��s�����֐� ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	SetOutApplicationLogValidFlag(FALSE);				//log.txtを出力しない
-	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
-	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
-	SetWindowStyleMode(GAME_WINDOW_BAR);				//タイトルバーはデフォルトにする
-	SetMainWindowText(TEXT(GAME_WINDOW_NAME));			//ウィンドウのタイトルの文字
-	SetAlwaysRunFlag(TRUE);								//非アクティブでも実行する
+	SetOutApplicationLogValidFlag(FALSE);				//log.txt���o�͂��Ȃ�
+	ChangeWindowMode(TRUE);								//�E�B���h�E���[�h�ɐݒ�
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//�w��̐��l�ŃE�B���h�E��\������
+	SetWindowStyleMode(GAME_WINDOW_BAR);				//�^�C�g���o�[�̓f�t�H���g�ɂ���
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));			//�E�B���h�E�̃^�C�g���̕���
+	SetAlwaysRunFlag(TRUE);								//��A�N�e�B�u�ł����s����
 
-	if (DxLib_Init() == -1) { return -1; }	//ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1) { return -1; }	//�c�w���C�u��������������
 
-	//動画の読み込み
+	//����̓ǂݍ���
 	handle = LoadGraph(MOVIE_PATH);
 
 
-	//無限ループ
+	//�������[�v
 	while (TRUE)
 	{
-		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
-		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+		if (ProcessMessage() != 0) { break; }	//���b�Z�[�W�����̌��ʂ��G���[�̂Ƃ��A�����I��
+		if (ClearDrawScreen() != 0) { break; }	//��ʂ������ł��Ȃ������Ƃ��A�����I��
 
 
 		if (GetMovieStateToGraph(handle) == 0)
 		{
-			SeekMovieToGraph(handle, 0);	//動画の再生バーを最初からにする
-			PlayMovieToGraph(handle);		//動画を再生状態にする
+			SeekMovieToGraph(handle, 0);	//����̍Đ��o�[���ŏ�����ɂ���
+			PlayMovieToGraph(handle);		//������Đ���Ԃɂ���
 
-			//動画の音量を調整（音の幅0～255）
+			//����̉��ʂ𒲐��i���̕�0�`255�j
 			SetMovieVolumeToGraph(127, handle);
 		}
 
-		//タイトル動画描画
+		//�^�C�g������`��
 		DrawGraph(0, 0, handle, FALSE);
-		DrawString(0,20,"猫の動画",GetColor(255,255,255));//動画の説明
+		DrawString(0,20,"�L�̓���",GetColor(255,255,255));//����̐���
 
 
-		DrawString(0, 0, "動画を再生しています・・・", GetColor(255, 255, 255));
+		DrawString(0, 0, "������Đ����Ă��܂��E�E�E", GetColor(255, 255, 255));
 
-		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+		ScreenFlip();		//���j�^�̃��t���b�V�����[�g�̑����ŗ���ʂ��ĕ`��
 	}
 
-	DxLib_End();	//ＤＸライブラリ使用の終了処理
+	DxLib_End();	//�c�w���C�u�����g�p�̏I������
 
 
-	DeleteGraph(handle);	//動画の削除
+	DeleteGraph(handle);	//����̍폜
 
 
 	return 0;
